e55e8469ac24f1703b1ba31911bb09eb3acb2b89
santiagodev28 <shurtado308@gmail.com>
2026-08-19 00:24:42 -0500

    fix(CORS): add fix cors origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'localhost:5173'
+    origin: 'http://localhost:5173'
   })
   await app.listen(process.env.PORT ?? 3000);
 }
